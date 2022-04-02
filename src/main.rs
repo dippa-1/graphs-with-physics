eb6d7d28e8640efb442df97884523370a8aaf787
dippa-1 <dominik.helfenstein@gmx.de>
2022-04-02 23:26:48 +0200

    update: atm not really possible to do graphs with bevy, because you can't have multiple entities of the same component in a bundle ...

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use bevy::prelude::*;
+
+// #[derive(Component)]
+// struct Vec3 {
+//     x: f32,
+//     y: f32,
+//     z: f32
+// }
+
+// impl Vec3 {
+//     fn new() -> Self {
+//         Self {
+//             x: 0.0,
+//             y: 0.0,
+//             z: 0.0,
+//         }
+//     }
+// }
+
+#[derive(Component)]
+struct Node(f32);
+
+#[derive(Component)]
+struct Edge;
+
+fn init_nodes(mut commands: Commands) {
+    println!("Init nodes...");
+    commands.spawn()
+        .insert_bundle((Node(1.0), Edge));
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Task: create a linear graph like N - N - N with the values 1 - 2 - 3
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(init_nodes)
+        .run();
 }
